4b9bc994acda50bd4cb990221ac0db54a5e540a8
AlbertLeipzig <albertclemvill@gmail.com>
2024-06-09 17:56:03 +0200

    last commit

--- server.js
+++ server.js
@@ -1,46 +1,54 @@
-import express from 'express';
+import express from "express";
+import fs from "fs";
+import { promisify } from "util";
 
-import books from './data/booksData.json' assert { type: 'json' };
-import customers from './data/customersData.json' assert { type: 'json' };
-import employees from './data/employeesData.json' assert { type: 'json' };
-import events from './data/eventsData.json' assert { type: 'json' };
-import messages from './data/messagesData.json' assert { type: 'json' };
-import products from './data/productsData.json' assert { type: 'json' };
-import tasks from './data/tasksData.json' assert { type: 'json' };
-import users from './data/usersData.json' assert { type: 'json' };
+const readFile = promisify(fs.readFile);
 
+async function loadJson(filename) {
+  const data = await readFile(filename, "utf8");
+  return JSON.parse(data);
+}
+
+const books = await loadJson("./data/booksData.json");
+const customers = await loadJson("./data/customersData.json");
+const employees = await loadJson("./data/employeesData.json");
+const events = await loadJson("./data/eventsData.json");
+const messages = await loadJson("./data/messagesData.json");
+const products = await loadJson("./data/productsData.json");
+const tasks = await loadJson("./data/tasksData.json");
+const users = await loadJson("./data/usersData.json");
 const app = express();
 const port = 8080;
 
-app.use('/books', (req, res) => {
+app.get("/books", (req, res) => {
   res.send(books);
 });
 
-app.use('/customers', (req, res) => {
+app.use("/customers", (req, res) => {
   res.send(customers);
 });
 
-app.use('/employees', (req, res) => {
+app.use("/employees", (req, res) => {
   res.send(employees);
 });
 
-app.use('/events', (req, res) => {
+app.use("/events", (req, res) => {
   res.send(events);
 });
 
-app.use('/messages', (req, res) => {
+app.use("/messages", (req, res) => {
   res.send(messages);
 });
 
-app.use('/products', (req, res) => {
+app.use("/products", (req, res) => {
   res.send(products);
 });
 
-app.use('/tasks', (req, res) => {
+app.use("/tasks", (req, res) => {
   res.send(tasks);
 });
 
-app.use('/users', (req, res) => {
+app.use("/users", (req, res) => {
   res.send(users);
 });
 
